0e3b438025fd062a66f935ae2935f3c69ed978ae
HDX007 <hecodextools@gmail.com>
2022-08-16 00:45:37 +0700

    first commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,21 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://pancakeswap.finance/',
+  },
+  {
+    label: 'Swap',
+    icon: 'TradeIcon',
+    href: '/swap',
+  },
+  {
+    label: 'Liquidity',
+    icon: 'PoolIcon',
+    href: '/pool',
+  },
+]
+
+export default config
